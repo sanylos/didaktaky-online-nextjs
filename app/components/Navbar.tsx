9988d5d157f4bf52f14db1bad8b8207d3f01acff
sanyl <homolkadam@seznam.cz>
2024-02-05 19:31:24 +0100

    Created navbar component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,48 @@
+'use client';
+import { useEffect } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import Link from 'next/link'
+import { useRouter, usePathname } from 'next/navigation';
+import styles from './Navbar.module.scss';
+
+const links = [
+    { label: 'Procvičování', href: '/' },
+    { label: 'Generování testů', href: '/' },
+];
+
+const Navbar = () => {
+    const path = usePathname();
+    const isLinkActive = (href) => { // returns true if pathname is equal to passed href
+        return path === href;
+    }
+
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+    }, []);
+
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-secondary-subtle">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="/">DIDAKTAKY - ONLINE</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            {links.map(({ href, label }) => (
+                                <li className="nav-item mx-1" key={href}>
+                                    <Link className={`nav-link ${isLinkActive(href) ? 'bg-secondary rounded text-light' : ''}`} href={href}>
+                                        {label}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
