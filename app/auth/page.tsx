5bb4d9b8e30dc1342340edc8d8d5fcd43e94f627
sanyl <homolkadam@seznam.cz>
2024-02-06 19:11:09 +0100

    Added form submit handle function

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,9 +1,38 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./page.scss"
+import { supabase } from "@/api";
 
 const Auth = () => {
     const [authType, setAuthType] = useState("login");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [someData, setSomeData] = useState([]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (authType == "register") {
+            const { data, error } = await supabase.auth.signUp({
+                email: email,
+                password: password,
+            })
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+            }
+        } else {
+            const { data, error } = await supabase.auth.signInWithPassword({
+                email: email,
+                password: password,
+            })
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(data);
+            }
+        }
+    }
 
     return <div className="background d-flex flex-row justify-content-center align-items-center">
         {
@@ -13,14 +42,14 @@ const Auth = () => {
                 <div className="card-body">
                     <h5 className="card-title text-center fw-medium">Přihlášení do systému DIDAKTAKY-ONLINE</h5>
                     <p className="card-text fst-italic">Přihlášením získáte přístup k interním funkcím systému!</p>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                            <input onChange={(e) => setEmail(e.target.value)} type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
                         </div>
                         <div className="mb-3">
                             <label htmlFor="exampleInputPassword1" className="form-label">Heslo</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                            <input onChange={(e) => setPassword(e.target.value)} type="password" className="form-control" id="exampleInputPassword1"></input>
                         </div>
                         <div className="d-flex flex-row justify-content-between align-items-center">
                             <a className="link-opacity-100-hover" onClick={() => setAuthType("register")}>Registrace</a>
@@ -37,7 +66,7 @@ const Auth = () => {
                 <div className="card-body">
                     <h5 className="card-title text-center fw-medium">Registrace do systému DIDAKTAKY-ONLINE</h5>
                     <p className="card-text fst-italic">Registrací získáte přístup k interním funkcím systému!</p>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3">
                             <label htmlFor="exampleInputEmail1" className="form-label">Email</label>
                             <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
