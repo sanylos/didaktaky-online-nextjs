dbb5f4cdd5dce6ec10756416fd968ad51a24d105
sanyl <homolkadam@seznam.cz>
2024-02-07 19:52:45 +0100

    Added UserProvider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
+import { UserProvider } from "./UserContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar></Navbar>
-        {children}</body>
+        <UserProvider>
+          <Navbar></Navbar>
+          {children}
+        </UserProvider>
+      </body>
     </html>
   );
 }
