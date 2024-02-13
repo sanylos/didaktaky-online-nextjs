8b9c6ec038b6e67dceaaab2fe7858ce9526d9db7
sanyl <homolkadam@seznam.cz>
2024-02-13 19:30:39 +0100

    Updated upsert query

--- app/utils/exerciseInsertion.ts
+++ app/utils/exerciseInsertion.ts
@@ -1,13 +1,17 @@
 import { supabase } from "@/api"
-export const upsertExercise = async (exercise: any) => {
+
+export const upsertExercise = async (exercise: any, answer: Array<string>, isAnswered: boolean, userAnswerId: string) => {
     const { data, error } = await supabase
         .from('userAnswers')
-        .insert({
+        .upsert({
+            'id': userAnswerId && userAnswerId,
             'exercise_id': exercise.exercise_id,
             'examType': exercise.test_type,
             'examSubject': exercise.subject,
             'exerciseType': exercise.type,
             'exerciseGroup': exercise.group,
+            'answer': answer,
+            'answered_at': isAnswered ? new Date() : null,
         })
         .select().single();
     return { data, error };
