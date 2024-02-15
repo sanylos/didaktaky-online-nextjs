b609b18cfaaa2a5e9d8c8d2318400648c794cb98
sanyl <homolkadam@seznam.cz>
2024-02-15 19:23:57 +0100

    Added available tests fetch

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,30 @@
+"use client"
+import { supabase } from "@/api";
+import { useEffect, useState } from "react";
 const Test = () => {
-    return <div>
-        Test page
-    </div>
-} 
+    const [availableTests, setAvailableTests] = useState<any[]>([]);
+
+    const fetchAvailableTests = async () => {
+        const { data, error } = await supabase
+            .from('tests')
+            .select('*');
+        if (data) {
+            setAvailableTests(data);
+            console.log(data);
+        }
+    }
+
+    useEffect(() => {
+        fetchAvailableTests();
+    }, []);
+
+    return <>
+        <div className="">
+            <div>
+                Test 1
+            </div>
+        </div>
+    </>
+}
 
 export default Test;
