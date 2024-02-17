1cbd5e0c389d2474085a61491913e4cbdc8379ba
sanyl <homolkadam@seznam.cz>
2024-02-17 21:12:16 +0100

    Added modal dismiss for button to start the test session

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -69,7 +69,7 @@ const TestPage = () => {
                         </div>
                         <div className="modal-footer d-flex justify-content-between">
                             <button type="button" className="btn btn-danger" onClick={cancelTestSession} disabled={!error && (!test?.exerciseCount || (loadedExercises < test?.exerciseCount))} data-bs-dismiss="modal">Zrušit</button>
-                            <button type="button" className="btn btn-success" onClick={e => setTestState("running")} disabled={!test?.exerciseCount || (loadedExercises < test?.exerciseCount)}>Začít</button>
+                            <button type="button" className="btn btn-success" onClick={e => setTestState("running")} disabled={!test?.exerciseCount || (loadedExercises < test?.exerciseCount)} data-bs-dismiss="modal">Začít</button>
                         </div>
                     </div>
                 </div>
