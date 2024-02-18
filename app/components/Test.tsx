abbd5c0f374efd2105b522944fac4b40c49ecb3c
sanyl <homolkadam@seznam.cz>
2024-02-18 19:38:18 +0100

    Added exercise rendering based on current index

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+//@ts-nocheck
+import Exercise from "./Exercise";
+import { useState } from "react";
 
-const Test = ({exercises}) => {
-  return (
-    <div>Test</div>
-  )
+const Test = ({ exercises }) => {
+    const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
+    const [answer, setAnswer] = useState([]);
+    const [answers, setAnswers] = useState([]);
+    console.log(exercises);
+    const handleAnswer = () => {
+
+    }
+    return (
+        <div>
+            <Exercise exercise={exercises[currentExerciseIndex]} answer={answer} handleAnswer={handleAnswer} isAnswered={false}/>
+        </div>
+    )
 }
 
 export default Test
