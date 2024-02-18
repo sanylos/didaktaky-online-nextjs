af3f12b5124cb29e31d41ee71b3e5a40ac6de9ef
sanyl <homolkadam@seznam.cz>
2024-02-18 19:54:49 +0100

    Added answers array initialization

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -1,14 +1,27 @@
 //@ts-nocheck
 import Exercise from "./Exercise";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Test = ({ exercises }) => {
     const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
     const [answers, setAnswers] = useState([]);
     console.log(exercises);
+    const initializeAnswers = () => {
+        let answersArray = [];
+        for (let i = 0; i < exercises.length; i++) {
+            let filledArray = Array(exercises[i].correct_answer.length).fill("");
+            answersArray[i] = filledArray;
+        }
+        setAnswers(answersArray);
+    }
+    console.log(answers);
+    useEffect(() => {
+        initializeAnswers();
+    }, [])
+
     const handleAnswer = (index, exerciseAnswer) => { //exercise answer is the passed answer from Exercise component
         let answerArray = [...answers];
-        answerArray[index] = exerciseAnswer;
+        answerArray[currentExerciseIndex][index] = exerciseAnswer;
         setAnswers(answerArray);
     }
     return (
