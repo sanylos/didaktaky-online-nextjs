6100db80bef5894360361201a37555fef5768a52
sanyl <homolkadam@seznam.cz>
2024-02-18 20:10:09 +0100

    Added answer selection trigger

--- app/components/Test.tsx
+++ app/components/Test.tsx
@@ -5,7 +5,6 @@ import { useEffect, useState } from "react";
 const Test = ({ exercises }) => {
     const [currentExerciseIndex, setCurrentExerciseIndex] = useState(0);
     const [answers, setAnswers] = useState([]);
-    console.log(exercises);
     const initializeAnswers = () => {
         let answersArray = [];
         for (let i = 0; i < exercises.length; i++) {
@@ -26,10 +25,10 @@ const Test = ({ exercises }) => {
     }
     return (
         <div>
-            <div className="d-flex justify-content-between bg-secondary w-auto" style={{overflowX: "auto"}}>
+            <div className="d-flex justify-content-between bg-secondary w-auto" style={{ overflowX: "auto" }}>
                 {Array.from({ length: exercises.length }, (_, i) => (
                     <div key={i} className="m-1 btn bg-secondary-subtle w-100 text-center">
-                        <span>{i + 1}</span>
+                        <span onClick={e => setCurrentExerciseIndex(i)}>{i + 1}</span>
                     </div>
                 ))}
             </div>
