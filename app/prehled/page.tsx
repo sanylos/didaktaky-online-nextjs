a7612aaf86dfec0c7ef7567266b9d34d92379337
sanyl <homolkadam@seznam.cz>
2024-02-24 18:59:25 +0100

    Added API call to fetch the test

--- app/prehled/page.tsx
+++ app/prehled/page.tsx
@@ -1,6 +1,25 @@
+//@ts-nocheck
+"use client";
+import { useEffect, useState } from "react";
+
 const Prehled = () => {
+    const [exercises, setExercises] = useState([]);
+
+    async function getData() {
+        const res = await fetch('/api/test/1',
+            {
+                method: 'GET',
+                next: { revalidate: 30 }
+            })
+        const data = await res.json();
+        return data;
+    }
+    useEffect(() => {
+        getData().then(data => setExercises(data));
+    }, [])
+
     return <div>
-        Prehled page
+        {JSON.stringify(exercises)}
     </div>
 }
 
