2fbb609f5b22a98c3e7d04f3ec2b2ac2ee899968
sanyl <homolkadam@seznam.cz>
2024-02-24 19:08:30 +0100

    Added API endpoint for getting user tests by userId

---
+++ app/api/user/[id]/tests/route.ts
@@ -0,0 +1,20 @@
+// @ts-nocheck
+import { NextResponse } from 'next/server';
+import { supabase } from "@/api";
+
+export async function GET(request: Request, context: any) {
+    const { params } = context;
+
+    const { data, error } = await supabase
+        .from('userTests')
+        .select('*')
+        .eq('user_id', params.id);
+
+    if (error) {
+        return NextResponse.error(new Error("Failed to fetch data."));
+    }
+
+    return NextResponse.json({
+        data
+    });
+}
