7119c4140841b05a3f6e63014670272ea7f5bfbe
sanyl <homolkadam@seznam.cz>
2024-02-24 20:10:24 +0100

    Added table responsivity

--- app/prehled/page.tsx
+++ app/prehled/page.tsx
@@ -28,28 +28,30 @@ const Prehled = () => {
     return <div>
         <div className="container bg-secondary-subtle mt-1 rounded p-2">
             <div className="fs-3 m-1 fw-bold d-flex align-items-center"><GrDocumentUser className="me-2 fs-5" /> Vaše testy</div>
-            <table className="table table-secondary table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">TYP TESTU</th>
-                        <th scope="col">PŘEDMĚT</th>
-                        <th scope="col">ZÍSKÁNO/MAXIMUM BODŮ</th>
-                        <th scope="col">ČAS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tests && tests.map((test, index) => (
-                        <tr key={index}>
-                            <td>{test.id}</td>
-                            <td>{getNameByShortcut(test.type)}</td>
-                            <td>{getNameByShortcut(test.subject)}</td>
-                            <td>{test.points}/{test.maxPoints}</td>
-                            <td>{((new Date(test.submitted_at).getTime() - new Date(test.created_at).getTime())/60000).toFixed(1)} min</td>
+            <div className="table-responsive">
+                <table className="table table-secondary table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">TYP TESTU</th>
+                            <th scope="col">PŘEDMĚT</th>
+                            <th scope="col">ZÍSKÁNO/MAXIMUM BODŮ</th>
+                            <th scope="col">ČAS</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                        {tests && tests.map((test, index) => (
+                            <tr key={index}>
+                                <td>{test.id}</td>
+                                <td>{getNameByShortcut(test.type)}</td>
+                                <td>{getNameByShortcut(test.subject)}</td>
+                                <td>{test.points}/{test.maxPoints}</td>
+                                <td>{((new Date(test.submitted_at).getTime() - new Date(test.created_at).getTime()) / 60000).toFixed(1)} min</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 }
