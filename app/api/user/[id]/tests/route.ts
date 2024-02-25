9acceb1467c62319c5a0679dd504d9ceb7f77d6a
sanyl <homolkadam@seznam.cz>
2024-02-25 19:04:18 +0100

    Added range API search parameter

--- app/api/user/[id]/tests/route.ts
+++ app/api/user/[id]/tests/route.ts
@@ -1,15 +1,18 @@
 // @ts-nocheck
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { supabase } from "@/api";
 
-export async function GET(request: Request, context: any) {
+export async function GET(request: NextRequest, context: any) {
     const { params } = context;
+    const searchParams = request.nextUrl.searchParams;
+    const range = searchParams.get('range');
 
     const { data, error } = await supabase
         .from('userTests')
         .select('*')
         .eq('user_id', params.id)
-        .order('created_at', {ascending: false})
+        .order('created_at', { ascending: false })
+        .range(0, range)
 
     if (error) {
         return NextResponse.error(new Error("Failed to fetch data."));
