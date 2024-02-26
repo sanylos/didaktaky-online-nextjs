cbdafefd910ac68986be790c72c8e94112e2c1d6
sanyl <homolkadam@seznam.cz>
2024-02-26 19:29:33 +0100

    Added test stats display

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -3,11 +3,12 @@
 
 import { supabase } from "@/api";
 import { useUser } from "@/app/UserContext";
+import { getNameByShortcut } from "@/app/utils/shortcutHandler";
 import { useEffect, useState } from "react";
 import { DiVim } from "react-icons/di";
 
 const UserTestPage = ({ params }: any) => {
-    const [tests, setTest] = useState();
+    const [test, setTest] = useState();
     const { userData } = useUser();
     const getTestById = async () => {
         const { data, error } = await supabase
@@ -26,13 +27,26 @@ const UserTestPage = ({ params }: any) => {
     }, [])
     return (
         <div>
-            <div className="fs-3 m-2">
-                Výsledek testu
-                <span class="ms-1 badge text-bg-secondary">#{params.id}</span>
-            </div>
-            {tests ?
+            <div className="fs-3 m-2 d-flex justify-content-between">
                 <div>
-                    {}
+                    Výsledek testu
+                    <span class="ms-1 badge text-bg-secondary">#{params.id}</span>
+                </div>
+                <div className="text-end">
+                    <span class="ms-1 badge text-bg-primary">{getNameByShortcut(test.type)}</span>
+                    <span class="ms-1 badge text-bg-primary">{getNameByShortcut(test.subject)}</span>
+                </div>
+            </div>
+            {test ?
+                <div>
+                    {test &&
+                        <div className="ms-3">
+                            <div>Úspěšnost: <span className="fw-bold">{(test.points / test.maxPoints) * 100}%</span></div>
+                            <div>Získáno: <span className="fw-bold">{test.points} bodů</span></div>
+                            <div>Maximum: <span className="fw-bold">{test.maxPoints} bodů</span></div>
+                            <div>Čas: <span className="fw-bold">{((new Date(test.submitted_at).getTime() - new Date(test.created_at).getTime()) / 60000).toFixed(1)} min</span></div>
+                        </div>
+                    }
                 </div>
                 :
                 <div>
