707a546035491cd387e6c8cc87ba44ea94722407
sanyl <homolkadam@seznam.cz>
2024-02-26 19:36:30 +0100

    Added exercises fetch

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -8,7 +8,8 @@ import { useEffect, useState } from "react";
 import { DiVim } from "react-icons/di";
 
 const UserTestPage = ({ params }: any) => {
-    const [test, setTest] = useState();
+    const [test, setTest] = useState({});
+    const [exercises, setExercises] = useState([]);
     const { userData } = useUser();
     const getTestById = async () => {
         const { data, error } = await supabase
@@ -22,19 +23,34 @@ const UserTestPage = ({ params }: any) => {
         }
         return data;
     }
+    const getExercisesByTestId = async () => {
+        if (test.id) {
+            const { data, error } = await supabase
+                .from('userAnswers')
+                .select('*')
+                .eq('userTest_id', test.id)
+                .order('generated_at', { ascending: true })
+            if (error) {
+                console.log(error);
+                return;
+            }
+            return data;
+        }
+    }
     useEffect(() => {
         getTestById().then((data) => { setTest(data) });
+        getExercisesByTestId().then((data) => { setExercises(data) });
     }, [])
     return (
         <div>
             <div className="fs-3 m-2 d-flex justify-content-between">
                 <div>
                     Výsledek testu
-                    <span class="ms-1 badge text-bg-secondary">#{params.id}</span>
+                    <span className="ms-1 badge text-bg-secondary">#{params.id}</span>
                 </div>
                 <div className="text-end">
-                    <span class="ms-1 badge text-bg-primary">{getNameByShortcut(test.type)}</span>
-                    <span class="ms-1 badge text-bg-primary">{getNameByShortcut(test.subject)}</span>
+                    <span className="ms-1 badge text-bg-primary">{getNameByShortcut(test.type)}</span>
+                    <span className="ms-1 badge text-bg-primary">{getNameByShortcut(test.subject)}</span>
                 </div>
             </div>
             {test ?
@@ -47,10 +63,11 @@ const UserTestPage = ({ params }: any) => {
                             <div>Čas: <span className="fw-bold">{((new Date(test.submitted_at).getTime() - new Date(test.created_at).getTime()) / 60000).toFixed(1)} min</span></div>
                         </div>
                     }
+                    {JSON.stringify(exercises)}
                 </div>
                 :
                 <div>
-                    <div class="alert alert-danger" role="alert">
+                    <div className="alert alert-danger" role="alert">
                         Tento test není veřejný! Pokud chcete tento test zobrazit, kontaktujte jeho vlastníka!
                     </div>
                 </div>}
