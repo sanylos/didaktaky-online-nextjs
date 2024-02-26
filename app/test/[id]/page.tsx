08bc7c71af4470806c78ced7ac3429ef13133ffd
sanyl <homolkadam@seznam.cz>
2024-02-26 18:57:11 +0100

    Added test fetch

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,6 +1,31 @@
+//@ts-nocheck
+"use client";
+
+import { supabase } from "@/api";
+import { useUser } from "@/app/UserContext";
+import { useEffect, useState } from "react";
+
 const UserTestPage = ({ params }: any) => {
+    const [test, setTest] = useState();
+    const { userData } = useUser();
+    const getTestById = async () => {
+        const { data, error } = await supabase
+            .from('userTests')
+            .select('*')
+            .eq('id', params.id)
+            .single()
+        if (error) {
+            console.log(error);
+            return;
+        }
+        return data;
+    }
+    useEffect(() => {
+        const data = getTestById().then((data) => { setTest(data) });
+    }, [])
     return (
-        <div>UserTestPage {params.id}</div>
+        <div>UserTestPage {params.id}
+            {JSON.stringify(test)}</div>
     )
 }
 
