3783c7662b5ec8209ccddadfa8ac5865556a2371
sanyl <homolkadam@seznam.cz>
2024-03-01 19:44:47 +0100

    Added tests fetching

--- app/prehled/tests/page.tsx
+++ app/prehled/tests/page.tsx
@@ -1,8 +1,33 @@
+// @ts-nocheck
 "use client"
+import { supabase } from "@/api";
+import { useUser } from "@/app/UserContext";
+import { useEffect, useState } from "react";
+
 const TestsPage = () => {
-  return (
-    <div>TestsPage</div>
-  )
+    const [tests, setTests] = useState([]);
+    const { userData } = useUser();
+    async function getUserTests() {
+        if (userData) {
+            const { data, error } = await supabase
+                .from('userTests')
+                .select('*')
+                .eq('user_id', userData.user.id)
+                .order('created_at', { ascending: false })
+                .range(tests.length, tests.length + 10)
+            let testsCopy = [...tests];
+            testsCopy.push(data);
+            setTests(testsCopy);
+        }
+    }
+    useEffect(() => {
+        getUserTests();
+    }, [userData])
+    return (
+        <div>{JSON.stringify(tests, 2, null)}
+            <button onClick={getUserTests}>load more</button>
+        </div>
+    )
 }
 
 export default TestsPage
