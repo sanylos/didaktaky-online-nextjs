3f2ee4553d64c78c8f5dee6a287baf3972e6763c
sanyl <homolkadam@seznam.cz>
2024-03-02 21:50:37 +0100

    Updated styles

--- app/prehled/page.tsx
+++ app/prehled/page.tsx
@@ -33,7 +33,7 @@ const Prehled = () => {
     }, [userData])
 
     return <div>
-        <div className="container bg-secondary-subtle mt-1 rounded p-2 d-flex">
+        <div className="container mt-1 rounded p-2 d-flex">
             <div className="rounded-circle d-flex justify-content-center align-items-center" style={{ width: "120px", height: "120px", backgroundColor: "gold" }}>
                 <span className="text-white fs-1">{userData?.user.email.slice(0, 2).toUpperCase()}</span>
             </div>
