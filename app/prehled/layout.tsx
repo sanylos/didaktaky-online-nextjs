3c735cd783ac9dda4e9bbaf83ab1daadb94d7b49
sanyl <homolkadam@seznam.cz>
2024-03-04 20:43:25 +0100

    Added dividing rows between cathegories

--- app/prehled/layout.tsx
+++ app/prehled/layout.tsx
@@ -92,6 +92,7 @@ export default function PrehledLayout({
                                 </button>
                             </Link>
                         ))}
+                        <hr />
                     </div>
                 ))}
             </div>
