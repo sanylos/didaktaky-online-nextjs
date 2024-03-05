a60ce6184967a2cd4ef20bf11302f92f7ab2964d
sanyl <homolkadam@seznam.cz>
2024-03-05 20:09:46 +0100

    Added uniqueLabels filtering and correct, incorrect counting

--- app/prehled/cviceni/prehled/page.tsx
+++ app/prehled/cviceni/prehled/page.tsx
@@ -1,6 +1,8 @@
+// @ts-nocheck
 "use client"
 import { supabase } from '@/api'
 import { useUser } from '@/app/UserContext'
+import { unique } from 'next/dist/build/utils'
 import { useSearchParams } from 'next/navigation'
 import { useEffect, useState } from 'react'
 
@@ -10,6 +12,27 @@ const CviceniPrehledPage = () => {
   const searchParams = useSearchParams();
   const examType = searchParams.get('zkouska');
   const subject = searchParams.get('predmet');
+  const [groupedData, setGroupedData] = useState();
+
+  const groupData = () => {
+    let correctGroup = [];
+    let incorrectGroup = [];
+    let labels = data.map(group => group.exercisegroup);
+    let uniqueLabels = [];
+    labels.forEach(label => {
+      if (!uniqueLabels.includes(label)) {
+        uniqueLabels.push(label);
+      }
+    });
+    uniqueLabels.forEach((label => {
+      const correctAnswerGroup = data.find((group) => group.exercisegroup === label && group.iscorrect == true);
+      const incorrectAnswerGroup = data.find((group) => group.exercisegroup === label && group.iscorrect == false);
+      if (correctAnswerGroup) { correctGroup.push(correctAnswerGroup.count); } else correctGroup.push(0);
+      if (incorrectAnswerGroup) { incorrectGroup.push(incorrectAnswerGroup.count); } else incorrectGroup.push(0);
+    }))
+
+    setGroupedData({ labels: uniqueLabels, correctCounts: correctGroup, incorrectCounts: incorrectGroup })
+  }
 
   const fetchExerciseGroups = async () => {
     const { data, error } = await supabase.rpc('getcountexercisegroups', {
@@ -26,9 +49,16 @@ const CviceniPrehledPage = () => {
       fetchExerciseGroups();
     }
   }, [userData, searchParams])
+
+  useEffect(() => {
+    if (data) {
+      groupData();
+    }
+  }, [data])
+
   return (
     <div>{JSON.stringify(data)}
-    
+      {JSON.stringify(groupedData)}
     </div>
   )
 }
