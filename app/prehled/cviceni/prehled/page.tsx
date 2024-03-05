d61bc6ecc095c08a354261247164812fa7e30914
sanyl <homolkadam@seznam.cz>
2024-03-05 19:48:32 +0100

    Added exercise group fetch

--- app/prehled/cviceni/prehled/page.tsx
+++ app/prehled/cviceni/prehled/page.tsx
@@ -1,11 +1,35 @@
 "use client"
+import { supabase } from '@/api'
+import { useUser } from '@/app/UserContext'
 import { useSearchParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
+
 const CviceniPrehledPage = () => {
+  const [data, setData] = useState();
+  const { userData } = useUser();
   const searchParams = useSearchParams();
   const examType = searchParams.get('zkouska');
   const subject = searchParams.get('predmet');
+
+  const fetchExerciseGroups = async () => {
+    const { data, error } = await supabase.rpc('getcountexercisegroups', {
+      user_id: userData.user.id
+    })
+      .eq('subject', subject)
+      .eq('examtype', examType)
+    console.log(data)
+    if (error) console.log(error);
+    if (data) setData(data);
+  }
+  useEffect(() => {
+    if (userData) {
+      fetchExerciseGroups();
+    }
+  }, [userData])
   return (
-    <div>{examType},{subject}</div>
+    <div>{JSON.stringify(data)}
+    
+    </div>
   )
 }
 
