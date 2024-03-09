d4f0d58d2bcfc8a8a419a1b6aff6524e119277a9
sanyl <homolkadam@seznam.cz>
2024-03-09 20:33:06 +0100

    Created statically generated content page

---
+++ app/ucebnice/[id]/page.tsx
@@ -0,0 +1,29 @@
+// @ts-nocheck
+import { supabase } from '@/api'
+import React from 'react'
+
+
+export async function generateStaticParams() {
+    return [{ id: 'Ernest-Hemingway' }]
+
+}
+
+export async function getContent(params) {
+    const { data, error } = await supabase
+        .from('cathegory_content')
+        .select('*')
+        .eq('id', params.id)
+        .single();
+    console.log(data);
+    console.log(error);
+    return data;
+}
+
+const AutorPage = async ({ params }) => {
+    const data = await getContent(params);
+    return (
+        <div>AutorPage {JSON.stringify(data)}</div>
+    )
+}
+
+export default AutorPage
