2db7ae41c6bbbcbc97ae4d859eaeaca5ae831a93
sanyl <homolkadam@seznam.cz>
2024-03-13 19:38:13 +0100

    Updated UI

--- app/ucebnice/[id]/page.tsx
+++ app/ucebnice/[id]/page.tsx
@@ -32,13 +32,13 @@ const AutorPage = async ({ params }) => {
     return (
         <div>
             {data?.ucebnice_content_articles?.map(article => (
-                <div key={article.id} id={article.id}>
+                <div style={{fontFamily: 'Roboto'}} key={article.id} id={article.id}>
                     <div className='d-flex flex-row align-items-center'>
-                        <a className='fs-3 text-secondary' href={'#' + article.id}>#</a>
-                        <h1 dangerouslySetInnerHTML={{ __html: article.title }}></h1>
+                        <a className='fs-1 text-secondary me-2 fw-bold' style={{textDecoration: 'none'}} href={'#' + article.id}>#</a>
+                        <h1 className='fw-semibold' dangerouslySetInnerHTML={{ __html: article.title }}></h1>
                     </div>
                     <h3 dangerouslySetInnerHTML={{ __html: article.subtitle }}></h3>
-                    <p style={{fontFamily: 'Roboto'}} dangerouslySetInnerHTML={{ __html: article.content }}></p>
+                    <p  dangerouslySetInnerHTML={{ __html: article.content }}></p>
                 </div>
             ))}
         </div >
