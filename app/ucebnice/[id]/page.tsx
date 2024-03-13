b2ea6d3f4c3403ccca87d7378afc3e2c0acb5410
sanyl <homolkadam@seznam.cz>
2024-03-13 19:47:58 +0100

    Updated query

--- app/ucebnice/[id]/page.tsx
+++ app/ucebnice/[id]/page.tsx
@@ -16,9 +16,11 @@ export async function getContent(params) {
         .from('ucebnice_category_content')
         .select('*, ucebnice_content_articles(*)')
         .eq('id', params.id)
+        .order('order_number', { referencedTable: 'ucebnice_content_articles', ascending: true })
         .single();
     console.log(data);
     if (error) {
+        console.log(error)
         redirect('/ucebnice')
     }
     return data;
@@ -32,13 +34,13 @@ const AutorPage = async ({ params }) => {
     return (
         <div>
             {data?.ucebnice_content_articles?.map(article => (
-                <div style={{fontFamily: 'Roboto'}} key={article.id} id={article.id}>
+                <div style={{ fontFamily: 'Roboto' }} key={article.id} id={article.id}>
                     <div className='d-flex flex-row align-items-center'>
-                        <a className='fs-1 text-secondary me-2 fw-bold' style={{textDecoration: 'none'}} href={'#' + article.id}>#</a>
+                        <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + article.id}>#</a>
                         <h1 className='fw-semibold' dangerouslySetInnerHTML={{ __html: article.title }}></h1>
                     </div>
                     <h3 dangerouslySetInnerHTML={{ __html: article.subtitle }}></h3>
-                    <p  dangerouslySetInnerHTML={{ __html: article.content }}></p>
+                    <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
                 </div>
             ))}
         </div >
