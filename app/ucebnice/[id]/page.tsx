4252d93f4c8cb18668e59d1b403caa51fc555caa
sanyl <homolkadam@seznam.cz>
2024-03-14 18:53:45 +0100

    Updated title tags

--- app/ucebnice/[id]/page.tsx
+++ app/ucebnice/[id]/page.tsx
@@ -37,9 +37,9 @@ const AutorPage = async ({ params }) => {
                 <div style={{ fontFamily: 'Roboto' }} key={article.id} id={article.id}>
                     <div className='d-flex flex-row align-items-center'>
                         <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + article.id}>#</a>
-                        <h1 className='fw-semibold' dangerouslySetInnerHTML={{ __html: article.title }}></h1>
+                        <h2 className='fw-semibold' dangerouslySetInnerHTML={{ __html: article.title }}></h2>
                     </div>
-                    <h3 dangerouslySetInnerHTML={{ __html: article.subtitle }}></h3>
+                    <h5 className="fst-italic" dangerouslySetInnerHTML={{ __html: article.subtitle }}></h5>
                     <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
                 </div>
             ))}
