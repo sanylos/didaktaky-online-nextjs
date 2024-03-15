828f10869fb9fec6de4dd15fe04a9c8f425f1b8b
sanyl <homolkadam@seznam.cz>
2024-03-15 18:54:35 +0100

    Updated layout structure

--- app/ucebnice/layout.tsx
+++ app/ucebnice/layout.tsx
@@ -2,11 +2,8 @@
 "use client"
 
 import "@/app/prehled/layout.scss"
-import { MdQueryStats } from "react-icons/md";
 import Link from "next/link";
-import { IoIosArrowForward } from "react-icons/io";
-import { RiHistoryFill } from "react-icons/ri";
-import { AiOutlineTrophy } from "react-icons/ai";
+import { FaAngleDown } from "react-icons/fa6";
 import { supabase } from "@/api";
 import { useEffect, useState } from "react";
 
@@ -35,24 +32,25 @@ export default function UcebniceLayout({
         <div className="d-flex flex-row">
             <div className="bg-secondary-subtle col-2 sidebar p-2 min-vh-100">
                 {data && data.map(category => (
-                    <div key={category.id}>
-                        <button className="fs-3 fw-bold btn p-0" data-bs-toggle="collapse" href={"#collapseCategories"+category.id} role="button" aria-expanded="false" aria-controls="collapseExample">
+                    <div key={category.id} className="rounded bg-light p-1 mb-2">
+                        <h3 className="fw-bold" data-bs-toggle="collapse" href={"#collapseCategories" + category.id} role="button" aria-expanded="false" aria-controls="collapseExample">
                             {category.name}
-                        </button>
-                        <div className="collapse" id={"collapseCategories"+category.id}>
+                        </h3>
+                        <div className="collapse" id={"collapseCategories" + category.id}>
                             {category.ucebnice_subcategories.map(subcategory => (
                                 <div key={subcategory.id}>
-                                    <button className="fs-5 fw-bold btn p-0" data-bs-toggle="collapse" href={"#collapseSubcategory"+subcategory.id} role="button" aria-expanded="false" aria-controls="collapseExample">
+                                    <h4 className="fs-5 fw-bold" data-bs-toggle="collapse" href={"#collapseSubcategory" + subcategory.id} role="button" aria-expanded="false" aria-controls="collapseExample">
                                         {subcategory.name}
-                                    </button>
-                                    <div className="collapse" id={"collapseSubcategory"+subcategory.id}>
+                                    </h4>
+                                    <div className="collapse" id={"collapseSubcategory" + subcategory.id}>
                                         {subcategory.ucebnice_category_content.map(content => (
                                             <div key={content.id}>
-                                                <Link href={'/ucebnice/' + content.id}>{content.name}</Link>
+                                                <h5>
+                                                    <Link style={{ textDecoration: 'none' }} href={'/ucebnice/' + content.id}>{content.name}</Link>
+                                                </h5>
                                             </div>
                                         ))}
                                     </div>
-                                    <hr></hr>
                                 </div>
                             ))}
                         </div>
