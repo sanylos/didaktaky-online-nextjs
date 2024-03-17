4027084e610a51927767471e422d4782cadda91a
sanyl <homolkadam@seznam.cz>
2024-03-17 19:25:37 +0100

    Added links to external data file

---
+++ app/data/links.ts
@@ -0,0 +1,7 @@
+export const links = [
+    { label: 'Procvičování', href: 'procvicovani' },
+    { label: 'Přehled', href: 'prehled' },
+    { label: 'Generování testů', href: 'test' },
+    { label: 'Učebnice', href: 'ucebnice' },
+    { label: 'Maturitní četba', href: 'cetba' },
+];
