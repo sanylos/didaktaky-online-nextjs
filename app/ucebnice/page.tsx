72b27836aa43211f273596d55851a0d949de8518
sanyl <homolkadam@seznam.cz>
2024-03-17 21:03:20 +0100

    Wrapped main content into div

--- app/ucebnice/page.tsx
+++ app/ucebnice/page.tsx
@@ -3,8 +3,10 @@ import './page.scss'
 const UcebnicePage = () => {
     return (
         <div className="d-flex flex-column main">
-            <h1 className='mb-0'>Elektronická učebnice</h1>
-            <h5>Hledáte komplexní přípravu na maturitní a přijímací zkoušky z češtiny, angličtiny a matematiky? Pak jste na správném místě! Nabízíme rozsáhlou sbírku materiálů, učebních osnov a témat, která vám pomohou dosáhnout vašich cílů.</h5>
+            <div className='p-1'>
+                <h1 className='mb-0'>Elektronická učebnice</h1>
+                <h5>Hledáte komplexní přípravu na maturitní a přijímací zkoušky z češtiny, angličtiny a matematiky? Pak jste na správném místě! Nabízíme rozsáhlou sbírku materiálů, učebních osnov a témat, která vám pomohou dosáhnout vašich cílů.</h5>
+            </div>
         </div>
     )
 }
