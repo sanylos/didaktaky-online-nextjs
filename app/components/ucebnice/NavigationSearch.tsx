e50a19f87527f61d2c475318af635c31dc3cb486
sanyl <homolkadam@seznam.cz>
2024-03-20 20:25:41 +0100

    Added filtering functionality

--- app/components/ucebnice/NavigationSearch.tsx
+++ app/components/ucebnice/NavigationSearch.tsx
@@ -1,15 +1,44 @@
 //@ts-nocheck
+"use client"
 import { FaSearch } from "react-icons/fa";
 import Navigation from "./Navigation"
+import { useEffect, useState } from "react";
+import Link from "next/link";
 
 const NavigationSearch = ({ data }) => {
+    const [filteredData, setFilteredData] = useState(data);
+    const [filter, setFilter] = useState("");
+    const getFilteredContent = () => {
+        let contentArray = [];
+        data.map((category) => (
+            category.ucebnice_subcategories.map((subcategory) => (
+                subcategory.ucebnice_category_content.map((content) => (
+                    content.name.includes(filter) ?
+                        contentArray.push(content) : console.log("neobsahuje " + filter)
+                ))
+            ))
+        ))
+        return contentArray;
+    }
+    useEffect(() => {
+        setFilteredData(getFilteredContent());
+    }, [filter])
     return (
-        <div className='container'>
+        <div>
             <div className="input-group mb-1">
-                <input type="text" className="form-control" placeholder="Hledat" aria-label="Vyhledavani" aria-describedby="button-addon1" />
+                <input onChange={e => setFilter(e.target.value)} type="text" className="form-control" placeholder="Hledat" aria-label="Vyhledavani" aria-describedby="button-addon1" />
                 <button className="btn btn-light" type="button" id="button-addon1"><FaSearch /></button>
             </div>
-            <Navigation data={data} name="UcebnicePage"></Navigation>
+            {!filter == "" &&
+                <div>
+                    {filteredData.map(content => (
+                        <div key={content.id}>
+                            <div className="d-flex aling-items-center">
+                                <h5><Link style={{ textDecoration: 'none' }} href={'/ucebnice/' + content.id}>{content.name}</Link></h5>
+                            </div>
+                        </div>
+                    ))}
+                </div>}
         </div>
     )
 }
