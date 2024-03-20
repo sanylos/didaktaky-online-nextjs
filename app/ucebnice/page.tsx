85292ad2a7b1773ffb2c3c8d1ba98917e969ba82
sanyl <homolkadam@seznam.cz>
2024-03-20 20:01:23 +0100

    Added searchbar

--- app/ucebnice/page.tsx
+++ app/ucebnice/page.tsx
@@ -3,6 +3,7 @@ import './page.scss'
 import { FaCheck } from "react-icons/fa";
 import { supabase } from '@/api';
 import Navigation from '../components/ucebnice/Navigation';
+import { FaSearch } from "react-icons/fa";
 
 export async function getContent() {
     const { data, error } = await supabase
@@ -22,7 +23,7 @@ export const UcebnicePage = async () => {
     return (
         <div className="d-flex flex-column main">
             <div className='p-1 shadow-lg mb-2'>
-                <h1 className='mb-0'>Elektronická učebnice</h1>
+                <h1 className='mb-0 fw-bold'>Elektronická učebnice</h1>
                 <h5>Hledáte komplexní přípravu na maturitní a přijímací zkoušky z češtiny, angličtiny a matematiky? Pak jste na správném místě! Nabízíme rozsáhlou sbírku materiálů, učebních osnov a témat, která vám pomohou dosáhnout vašich cílů.</h5>
             </div>
             <div className='p-1'>
@@ -42,6 +43,10 @@ export const UcebnicePage = async () => {
             <div className='mt-3 p-1'>
                 <h2 className='text-center'>Obsah učebnice</h2>
                 <div className='container'>
+                    <div className="input-group mb-1">
+                        <input type="text" className="form-control" placeholder="Hledat" aria-label="Vyhledavani" aria-describedby="button-addon1" />
+                        <button className="btn btn-light" type="button" id="button-addon1"><FaSearch /></button>
+                    </div>
                     <Navigation data={data} name="UcebnicePage"></Navigation>
                 </div>
             </div>
