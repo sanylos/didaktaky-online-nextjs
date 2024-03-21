80058615afa2f1949eb3a8acce6f7f03dfb3f620
sanyl <homolkadam@seznam.cz>
2024-03-21 20:06:14 +0100

    Added userAnswerId check istead of passing isAnswered boolean

--- app/utils/exerciseInsertion.ts
+++ app/utils/exerciseInsertion.ts
@@ -1,20 +1,32 @@
 import { supabase } from "@/api"
 import { validateAnswer } from "./answerValidation";
 
-export const upsertExercise = async (exercise: any, answer: Array<string>, isAnswered: boolean, userAnswerId: string) => {
-    const { data, error } = await supabase
-        .from('userAnswers')
-        .upsert({
-            'id': userAnswerId && userAnswerId,
-            'exercise_id': exercise.exercise_id,
-            'examType': exercise.test_type,
-            'examSubject': exercise.subject,
-            'type': exercise.type,
-            'exerciseGroup': exercise.group,
-            'answer': answer,
-            'answered_at': isAnswered ? new Date() : null,
-            'points': [validateAnswer(exercise, answer), exercise.points]
-        })
-        .select().single();
-    return { data, error };
+export const upsertExercise = async (exercise: any, answer: Array<string>, userAnswerId: string) => {
+    console.log(userAnswerId)
+    if (userAnswerId) {
+        const { data, error } = await supabase
+            .from('userAnswers')
+            .update({
+                'answer': answer,
+                'points': [validateAnswer(exercise, answer), exercise.points],
+                'answered_at': new Date()
+            })
+            .eq('id', userAnswerId)
+            .select().single();
+        return { data, error };
+    } else {
+        const { data, error } = await supabase
+            .from('userAnswers')
+            .insert({
+                'exercise_id': exercise.exercise_id,
+                'examType': exercise.test_type,
+                'examSubject': exercise.subject,
+                'type': exercise.type,
+                'exerciseGroup': exercise.group,
+                'answer': answer,
+                'points': [validateAnswer(exercise, answer), exercise.points]
+            })
+            .select().single();
+        return { data, error };
+    }
 }
