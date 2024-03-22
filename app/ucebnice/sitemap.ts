2309ee0642f5625146e59c6a9057bb7b8663b9a7
sanyl <homolkadam@seznam.cz>
2024-03-22 19:48:05 +0100

    Updated sitemap generation

--- app/ucebnice/sitemap.ts
+++ app/ucebnice/sitemap.ts
@@ -1,14 +1,20 @@
+// @ts-nocheck
 import { supabase } from '@/api'
 import { MetadataRoute } from 'next'
 
 export default async function sitemap() {
-    const { data, error } = await supabase
-        .from('ucebnice_category_content')
-        .select('id')
-    console.log(data)
-    console.log(error)
-    return data?.map((site) => ({
-        url: process.env.NEXT_PUBLIC_PRODUCTION_URL + '/ucebnice/' + site.id,
-        lastModified: new Date(),
-    }))
+    const { data } = await supabase
+        .from('ucebnice_categories')
+        .select('*, ucebnice_subcategories(*, ucebnice_category_content(*))')
+
+    const urls = [];
+    data.map(category => {
+        category.ucebnice_subcategories.map(subcategory => {
+            subcategory.ucebnice_category_content.map(content => {
+                const url = process.env.NEXT_PUBLIC_PRODUCTION_URL + '/ucebnice/' + category.id + '/' + subcategory.id + '/' + content.id;
+                urls.push({ url });
+            });
+        });
+    });
+    return urls
 }
