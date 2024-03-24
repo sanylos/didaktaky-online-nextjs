8e059cfc039ead83ebbbac5c22d794190bf0f1ff
sanyl <homolkadam@seznam.cz>
2024-03-24 20:19:54 +0100

    Added navigation and search

--- app/ucebnice/[category_id]/page.tsx
+++ app/ucebnice/[category_id]/page.tsx
@@ -1,6 +1,8 @@
 // @ts-nocheck
 import { supabase } from '@/api'
 import { redirect } from 'next/navigation'
+import NavigationSearch from '@/app/components/ucebnice/NavigationSearch';
+import Navigation from '@/app/components/ucebnice/Navigation';
 
 
 export async function generateStaticParams() {
@@ -47,15 +49,19 @@ export const revalidate = 60;
 
 const CategoryPage = async ({ params }) => {
     const data = await getContent(params);
-    console.log(data.ucebnice_subcategories)
+    console.log(data)
     return (
         <div className='container' style={{ fontFamily: 'Roboto' }}>
             <div className='row'>
                 <h1 className='fw-semibold mb-0'>{data?.name}</h1>
                 <h4 className='fw-normal'>{data?.description}</h4>
             </div>
-            <div className='row'>
+            <div className='row bg-secondary-subtle mx-1 rounded'>
                 <h2 className='fw-semibold mb-0'>Obsah učebnice</h2>
+                <div>
+                    <NavigationSearch data={new Array(data)} />
+                    <Navigation data={new Array(data)} isCollapsed={false} />
+                </div>
             </div>
         </div>
     )
