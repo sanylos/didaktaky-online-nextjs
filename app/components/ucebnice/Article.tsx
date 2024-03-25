2c1a2bd58680891708672a1028020671032be341
sanyl <homolkadam@seznam.cz>
2024-03-25 19:36:17 +0100

    Updated ArticleTitle id and title prop

--- app/components/ucebnice/Article.tsx
+++ app/components/ucebnice/Article.tsx
@@ -4,7 +4,7 @@ import ArticleTitle from "./ArticleTitle"
 const Article = ({ article }) => {
     return (
         <div id={article.id} style={{ fontFamily: 'Roboto', fontSize: '1.125rem' }}>
-            <ArticleTitle article={article} />
+            <ArticleTitle id={article.id} title={article.title} />
             <h5 className="fst-italic" dangerouslySetInnerHTML={{ __html: article.subtitle }}></h5>
             <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
         </div>
