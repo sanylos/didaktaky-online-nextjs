f52b96d2956da884c9e4953a7f3a9e543816e9f7
sanyl <homolkadam@seznam.cz>
2024-03-27 19:28:25 +0100

    Added subtitle prop

--- app/components/ucebnice/ArticleTitle.tsx
+++ app/components/ucebnice/ArticleTitle.tsx
@@ -1,10 +1,13 @@
 //@ts-nocheck
 
-const ArticleTitle = ({ id, title }) => {
+const ArticleTitle = ({ id, title, subtitle }) => {
     return (
-        <div className='d-flex flex-row align-items-center'>
-            <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + id}>#</a>
-            <h2 className='fw-semibold' dangerouslySetInnerHTML={{ __html: title }}></h2>
+        <div className='d-flex flex-row align-items-center justify-content-between'>
+            <div className="d-flex flex-row align-items-center col-6">
+                <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + id}>#</a>
+                <h2 className='fw-semibold' dangerouslySetInnerHTML={{ __html: title }}></h2>
+            </div>
+            <h5 className="fst-italic col-6 text-break text-end" dangerouslySetInnerHTML={{ __html: subtitle ? subtitle : "" }}></h5>
         </div>
     )
 }
