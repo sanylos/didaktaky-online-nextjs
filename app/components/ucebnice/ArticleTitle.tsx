2846f63e2a24ec33dc25ac9dda10b79c4af5ef00
sanyl <homolkadam@seznam.cz>
2024-03-27 19:36:56 +0100

    Added horizontal row

--- app/components/ucebnice/ArticleTitle.tsx
+++ app/components/ucebnice/ArticleTitle.tsx
@@ -2,13 +2,16 @@
 
 const ArticleTitle = ({ id, title, subtitle }) => {
     return (
-        <div className='d-flex flex-row align-items-center justify-content-between'>
-            <div className="d-flex flex-row align-items-center col-6">
-                <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + id}>#</a>
-                <h2 className='fw-semibold' dangerouslySetInnerHTML={{ __html: title }}></h2>
+        <>
+            <div className='d-flex flex-row align-items-center justify-content-between'>
+                <div className="d-flex flex-row align-items-center col-6">
+                    <a className='fs-1 text-secondary me-2 fw-bold' style={{ textDecoration: 'none' }} href={'#' + id}>#</a>
+                    <h2 className='fw-semibold' dangerouslySetInnerHTML={{ __html: title }}></h2>
+                </div>
+                <h5 className="fst-italic col-6 text-break text-end" dangerouslySetInnerHTML={{ __html: subtitle ? subtitle : "" }}></h5>
             </div>
-            <h5 className="fst-italic col-6 text-break text-end" dangerouslySetInnerHTML={{ __html: subtitle ? subtitle : "" }}></h5>
-        </div>
+            <hr className="mt-0 text-danger bg-danger" style={{height: '1.5px'}} />
+        </>
     )
 }
 
