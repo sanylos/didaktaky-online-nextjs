635790ca5675159dee30d5a8f36e253978e3a239
sanyl <homolkadam@seznam.cz>
2024-03-27 19:28:40 +0100

    Moved subtitle to article title component

--- app/components/ucebnice/Article.tsx
+++ app/components/ucebnice/Article.tsx
@@ -4,8 +4,7 @@ import ArticleTitle from "./ArticleTitle"
 const Article = ({ article }) => {
     return (
         <div id={article.id} style={{ fontFamily: 'Roboto', fontSize: '1.125rem' }}>
-            <ArticleTitle id={article.id} title={article.title} />
-            <h5 className="fst-italic" dangerouslySetInnerHTML={{ __html: article.subtitle }}></h5>
+            <ArticleTitle id={article.id} title={article.title} subtitle={article.subtitle} />
             <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
         </div>
     )
