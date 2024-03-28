3e0c04e3f24f5ed7e3026f33d5d1e0acd28d3d76
sanyl <homolkadam@seznam.cz>
2024-03-28 19:53:43 +0100

    Created reusable card

---
+++ app/components/UI/RoundedCard.tsx
@@ -0,0 +1,16 @@
+import { FaCheck } from "react-icons/fa";
+const RoundedCard = ({ title, text }: { title: string, text: string }) => {
+    return (
+        <div className="box text-white mb-1 mx-1">
+            <div className="ms-1 box__icon">
+                <FaCheck className='text-success fs-4' />
+            </div>
+            <div className='d-flex flex-column'>
+                <h5 className="box__title ms-1 mb-0">{title}</h5>
+                <p className="ms-1 mb-0">{text}</p>
+            </div>
+        </div>
+    )
+}
+
+export default RoundedCard
