66b09178cdba62ee186d9969b5c7d837967f9245
sanyl <homolkadam@seznam.cz>
2024-03-28 20:12:12 +0100

    Added cards to display options

--- app/procvicovani/page.tsx
+++ app/procvicovani/page.tsx
@@ -1,5 +1,7 @@
 import RoundedCard from "../components/UI/RoundedCard"
 
+
+
 const ProcvicovaniPage = () => {
     const pros = [
         { title: 'Zvýšení šancí na úspěch', text: 'Naše platforma vám pomůže dosáhnout vynikajících výsledků v přijímacích zkouškách a maturitách.' },
@@ -18,6 +20,22 @@ const ProcvicovaniPage = () => {
                 {pros.map((item, index) => (
                     <RoundedCard key={index} title={item.title} text={item.text} />
                 ))}
+            </div>
+            <div className="d-flex justify-content-around mt-5">
+                <div>
+                    <div className="card bg-secondary-subtle border-0 w-auto" style={{ width: '18rem' }}>
+                        <div className="card-body">
+                            <h5 className="card-title">PŘIJÍMACÍ ZKOUŠKY</h5>
+                            <h6 className="card-subtitle mb-2 text-body-secondary">PŘEDMĚTY</h6>
+                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" className="card-link">Card link</a>
+                            <a href="#" className="card-link">Another link</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    CS
+                </div>
             </div>
         </div>
     )
