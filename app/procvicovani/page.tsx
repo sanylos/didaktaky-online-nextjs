3a45377b4228ee773fbfedb8dc04109c5508e005
sanyl <homolkadam@seznam.cz>
2024-03-28 20:02:38 +0100

    Updated html structure

--- app/procvicovani/page.tsx
+++ app/procvicovani/page.tsx
@@ -9,9 +9,11 @@ const ProcvicovaniPage = () => {
     ]
     return (
         <div>
-            <h1>Získejte náskok na přijímací zkoušky a maturity s naší platformou.</h1>
-            <h3 className='fw-normal fs-4'>Přijímací zkoušky a maturity představují důležité milníky v životě studenta. Úspěšné zvládnutí těchto testů otevírá dveře k vysněným oborům a kariérám. Naše webová aplikace vám pomůže dosáhnout vašich cílů a snů s maximální efektivitou a pohodlím.</h3>
-            <div className="">
+            <div className="container-fluid">
+                <h1>Získejte náskok na přijímací zkoušky a maturity s naší platformou.</h1>
+                <h3 className='fw-normal fs-4'>Přijímací zkoušky a maturity představují důležité milníky v životě studenta. Úspěšné zvládnutí těchto testů otevírá dveře k vysněným oborům a kariérám. Naše webová aplikace vám pomůže dosáhnout vašich cílů a snů s maximální efektivitou a pohodlím.</h3>
+            </div>
+            <div className="container-fluid">
                 {pros.map((item, index) => (
                     <RoundedCard key={index} title={item.title} text={item.text} />
                 ))}
