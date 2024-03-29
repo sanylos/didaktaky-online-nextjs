35cef884871b1d42ac8dc82095746374b133fbb5
sanyl <homolkadam@seznam.cz>
2024-03-29 20:00:22 +0100

    Added conditional rendering based on filter state

--- app/procvicovani/page.tsx
+++ app/procvicovani/page.tsx
@@ -2,8 +2,10 @@
 import RoundedCard from "../components/UI/RoundedCard"
 import { FaCircleArrowRight } from "react-icons/fa6";
 import SelectionCard from "../components/procvicovani/SelectionCard";
+import { useState } from "react";
 
 const ProcvicovaniPage = () => {
+    const [filter, setFilter] = useState(null);
     const pros = [
         { title: 'Zvýšení šancí na úspěch', text: 'Naše platforma vám pomůže dosáhnout vynikajících výsledků v přijímacích zkouškách a maturitách.' },
         { title: 'Získání sebejistoty', text: 'Trénink s reálnými otázkami a pod tlakem časového limitu vám dodá potřebnou jistotu pro zvládnutí zkoušky.' },
@@ -11,8 +13,8 @@ const ProcvicovaniPage = () => {
         { title: 'Lepší zvládnutí stresu', text: 'Simulace reálných podmínek zkoušky vám pomůže zvládat stres a lépe se soustředit v kritické situaci.' },
     ]
     const options = {
-        PZ: ['Český jazyk a literatura', 'Matematika'],
-        MZ: ['Český jazyk a literatura', 'Matematika', 'Anglický jazyk']
+        PZ: ['CJL', 'MAT'],
+        MZ: ['CJL', 'MAT', 'ANJ']
     }
 
     const handleFilter = () => {
@@ -21,25 +23,29 @@ const ProcvicovaniPage = () => {
 
     return (
         <div>
-            <div className="container-fluid">
-                <h1 className="m-0">Získejte náskok na přijímací zkoušky a maturity s naší platformou.</h1>
-                <hr className="m-0 text-danger bg-danger" style={{ height: '2px' }} />
-                <h3 className='fw-normal fs-4'>Přijímací zkoušky a maturity představují důležité milníky v životě studenta. Úspěšné zvládnutí těchto testů otevírá dveře k vysněným oborům a kariérám. Naše webová aplikace vám pomůže dosáhnout vašich cílů a snů s maximální efektivitou a pohodlím.</h3>
-            </div>
-            <div className="container-fluid">
-                {pros.map((item, index) => (
-                    <RoundedCard key={index} title={item.title} text={item.text} />
-                ))}
-            </div>
-            <div className="d-flex justify-content-around flex-wrap flex-lg-nowrap mt-5">
-                <div className="container mb-1">
-                    <SelectionCard title="PŘIJÍMACÍ ZKOUŠKA" options={options['PZ']} handleFilter={handleFilter} />
+            {!filter ? <div>
+                <div className="container-fluid">
+                    <h1 className="m-0">Získejte náskok na přijímací zkoušky a maturity s naší platformou.</h1>
+                    <hr className="m-0 text-danger bg-danger" style={{ height: '2px' }} />
+                    <h3 className='fw-normal fs-4'>Přijímací zkoušky a maturity představují důležité milníky v životě studenta. Úspěšné zvládnutí těchto testů otevírá dveře k vysněným oborům a kariérám. Naše webová aplikace vám pomůže dosáhnout vašich cílů a snů s maximální efektivitou a pohodlím.</h3>
                 </div>
-                <div className="container mb-1">
-                    <SelectionCard title="MATURITNÍ ZKOUŠKA" options={options['MZ']} handleFilter={handleFilter} />
+                <div className="container-fluid">
+                    {pros.map((item, index) => (
+                        <RoundedCard key={index} title={item.title} text={item.text} />
+                    ))}
+                </div>
+                <div className="d-flex justify-content-around flex-wrap flex-lg-nowrap mt-5">
+                    <div className="container mb-1">
+                        <SelectionCard title="PŘIJÍMACÍ ZKOUŠKA" key="PZ" options={options['PZ']} handleFilter={handleFilter} />
+                    </div>
+                    <div className="container mb-1">
+                        <SelectionCard title="MATURITNÍ ZKOUŠKA" key="MZ" options={options['MZ']} handleFilter={handleFilter} />
+                    </div>
                 </div>
             </div>
-        </div>
+                :
+                ''}
+        </div >
     )
 }
 
