9a081eb43c537cbb3345baa34d6b893fb3009014
sanyl <homolkadam@seznam.cz>
2024-03-30 19:08:12 +0100

    Added simple HTML structure

--- app/admin/exercises/add/page.tsx
+++ app/admin/exercises/add/page.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+//@ts-nocheck
+"use client"
+import Exercise from "@/app/components/Exercise"
+import { useState } from "react"
 const ExerciseAddPage = () => {
-  return (
-    <div>ExerciseAddPage</div>
-  )
+    const [exercise, setExercise] = useState({})
+    return (
+        <div>
+            <h1>Přidat cvičení</h1>
+            <div>
+                <div>
+                    
+                </div>
+                <div>
+                    <h2>Náhled</h2>
+                    <Exercise exercise={exercise} isAnswered={true} showExerciseNumber={true} />
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ExerciseAddPage
