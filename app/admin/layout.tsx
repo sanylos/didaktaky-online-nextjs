6a874e84cd7f02bb0a75286503590705f514a395
sanyl <homolkadam@seznam.cz>
2024-04-04 19:27:31 +0200

    Added navigation links

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Link from "next/link";
 export default function AdminLayout({
     children,
 }: Readonly<{
@@ -7,8 +8,10 @@ export default function AdminLayout({
 
     return (
         <div className="d-flex flex-row">
-            <div className="bg-secondary-subtle col-2 sidebar p-2 min-vh-100">
-
+            <div className="bg-secondary-subtle col-2 sidebar p-2 min-vh-100 d-flex flex-column">
+                <Link href="/admin">Přehled</Link>
+                <Link href="/admin/tests">Testy</Link>
+                <Link href="/admin/exercises">Cvičení</Link>
             </div>
             <div className="col-10 children" style={{ maxWidth: '100vw' }}>
                 {children}
