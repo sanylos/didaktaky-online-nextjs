98dcc3a88718c2afeaf51552f5facdab7a2df6b7
sanyl <homolkadam@seznam.cz>
2024-04-04 19:36:52 +0200

    Added table

--- app/admin/tests/page.tsx
+++ app/admin/tests/page.tsx
@@ -1,8 +1,48 @@
-import React from 'react'
+//@ts-nocheck
+"use client"
+import { supabase } from '@/api';
+import React, { useEffect, useState } from 'react'
 
 const TestsPage = () => {
+    const [tests, setTests] = useState<any>([]);
+    const fetchTests = async () => {
+        const { data, error } = await supabase
+            .from('tests')
+            .select('*')
+        if (error) console.log(error)
+        setTests(data);
+    }
+    useEffect(() => {
+        fetchTests();
+    }, [])
     return (
-        <div>TestsPage</div>
+        <div>
+            <h1>Testy</h1>
+            <table className="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">TYP</th>
+                        <th scope="col">PŘEDMĚT</th>
+                        <th scope="col">VARIANTA</th>
+                        <th scope="col">ČAS</th>
+                        <th scope="col">CVIČENÍ</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tests?.map(test => (
+                        <tr key={test.id}>
+                            <td>{test.id}</td>
+                            <td>{test.type}</td>
+                            <td>{test.subject}</td>
+                            <td>{test.variant}</td>
+                            <td>{test.duration}</td>
+                            <td>{test.exerciseCount}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
