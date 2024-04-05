8dc8fcd26161b0046aba01d193db8f5404e2c061
sanyl <homolkadam@seznam.cz>
2024-04-05 20:01:50 +0200

    Added test type field

--- app/admin/tests/add/page.tsx
+++ app/admin/tests/add/page.tsx
@@ -1,10 +1,25 @@
+//@ts-nocheck
 "use client"
 
+import { useState } from "react"
+
 const AddTestPage = () => {
+    const [test, setTest] = useState({});
+    const saveTest = (key, value) => {
+        setTest({ ...test, [key]: value })
+    }
     return (
         <div>
             <h1>Přidat test</h1>
-
+            {JSON.stringify(test)}
+            <div>
+                Typ testu
+                <select onChange={e => saveTest("type", e.target.value)}>
+                    <option value="PZ">PZ</option>
+                    <option value="PZ">MZ</option>
+                </select>
+            </div>
+            
         </div>
     )
 }
