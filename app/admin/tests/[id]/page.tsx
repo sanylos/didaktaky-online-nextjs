5c9450bbc6e2faac741e0e73b1f96f8539c95daa
sanyl <homolkadam@seznam.cz>
2024-04-06 19:51:06 +0200

    Created test detail page

---
+++ app/admin/tests/[id]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { supabase } from "@/api"
+
+const TestPreviewPage = ({ params }) => {
+    
+    return (
+        <div>TestPreviewPage</div>
+    )
+}
+
+export default TestPreviewPage
