2cce190dc28dd5ecddda285ce7578356c2d5b37d
sanyl <homolkadam@seznam.cz>
2024-04-06 19:59:41 +0200

    Improted exercise component

--- app/admin/tests/[id]/page.tsx
+++ app/admin/tests/[id]/page.tsx
@@ -1,8 +1,8 @@
 //@ts-nocheck
 "use client"
-
+import Exercise from "@/app/components/Exercise"
 import { supabase } from "@/api"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 const TestPreviewPage = ({ params }) => {
     const [exercises, setExercises] = useState([]);
@@ -11,11 +11,22 @@ const TestPreviewPage = ({ params }) => {
             .from('exercises')
             .select('*')
             .eq('test_id', params.id)
+        if (error) console.log(error);
+        console.log(data)
+        setExercises(data);
     }
-    fetchExercises();
+
+    useEffect(() => {
+        fetchExercises();
+    }, [])
     return (
         <div>TestPreviewPage
-            
+            {exercises && exercises.map((exercise, index) => (
+                <div key={index}>
+                    <hr />
+                    <Exercise exercise={exercise} answer={exercise.correct_answer} handleAnswer={() => { }} showExerciseNumber={true} />
+                </div>
+            ))}
         </div>
     )
 }
