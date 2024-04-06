e45d89c1b1eb84b86adbd514833e772a02e0b966
sanyl <homolkadam@seznam.cz>
2024-04-06 19:52:47 +0200

    Added test fetch

--- app/admin/tests/[id]/page.tsx
+++ app/admin/tests/[id]/page.tsx
@@ -1,11 +1,22 @@
+//@ts-nocheck
 "use client"
 
 import { supabase } from "@/api"
+import { useState } from "react"
 
 const TestPreviewPage = ({ params }) => {
-    
+    const [exercises, setExercises] = useState([]);
+    const fetchExercises = async () => {
+        const { data, error } = await supabase
+            .from('exercises')
+            .select('*')
+            .eq('test_id', params.id)
+    }
+    fetchExercises();
     return (
-        <div>TestPreviewPage</div>
+        <div>TestPreviewPage
+            
+        </div>
     )
 }
 
