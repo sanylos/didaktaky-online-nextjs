cc6be3c85a7315eb635335e3e552e454cce90176
sanyl <homolkadam@seznam.cz>
2024-04-07 19:22:05 +0200

    Added auth check

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,28 @@
+//@ts-nocheck
 "use client"
+import { supabase } from "@/api";
 import Link from "next/link";
+import { notFound, redirect, useRouter } from "next/navigation";
+import { useEffect } from "react";
 export default function AdminLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
+    const router = useRouter();
+    useEffect(() => {
+        const checkUser = async () => {
+            const { data, error } = await supabase.auth.getUser();
+            if (error) console.log(error);
+            else return data;
+            return null;
+        }
+        checkUser().then((data) => {
+            if (data?.user?.email !== 'admin@sanyl.cz') {
+                router.replace('/');
+            };
+        });
+    }, [])
     return (
         <div className="d-flex flex-row">
             <div className="bg-secondary-subtle col-2 sidebar p-2 min-vh-100 d-flex flex-column">
