a3c1a3a7c9e0142ee8ac259ed250be35a403d484
sanyl <homolkadam@seznam.cz>
2024-04-07 19:44:32 +0200

    Created title and add button

--- app/admin/exercises/page.tsx
+++ app/admin/exercises/page.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import Link from "next/link"
+
 const ExercisesPage = () => {
     return (
-        <div>ExercisesPage</div>
+        <div className='d-flex justify-content-between align-items-center'>
+            <h1>Cvičení</h1>
+            <Link href="/admin/exercises/add">
+                <button className='btn btn-info p-3 m-1'>Přidat nové cvičení</button>
+            </Link>
+        </div>
     )
 }
 
