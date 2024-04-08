11de6ee194a86d59bd727439d0168db9005f95e7
sanyl <homolkadam@seznam.cz>
2024-04-08 19:36:17 +0200

    Added categories fetch

--- app/admin/textbook/page.tsx
+++ app/admin/textbook/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client"
+
+import { supabase } from "@/api";
+import { useEffect, useState } from "react"
 
 const TextbookPage = () => {
-  return (
-    <div>TextbookPage</div>
-  )
+    const [categories, setCategories] = useState<any>([]);
+    const fetchCategories = async () => {
+        const { data, error } = await supabase
+            .from('ucebnice_category_content')
+            .select('*')
+        if (error) alert(error.message);
+        setCategories(data);
+    }
+    useEffect(() => {
+        fetchCategories();
+    }, [])
+    return (
+        <div>
+            <h1>Kategorie</h1>
+            {JSON.stringify(categories)}
+        </div>
+    )
 }
 
 export default TextbookPage
