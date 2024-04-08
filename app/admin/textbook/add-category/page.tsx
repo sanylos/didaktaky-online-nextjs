fa0cebd11b0895fc1bfc7ba9b592c4da769d1f3b
sanyl <homolkadam@seznam.cz>
2024-04-08 19:47:10 +0200

    Added object creating method

--- app/admin/textbook/add-category/page.tsx
+++ app/admin/textbook/add-category/page.tsx
@@ -1,10 +1,14 @@
+//@ts-nocheck
 "use client"
 
 const CategoryAddPage = () => {
+    const saveSubcategory = (key, value) => {
+        setTest({ ...test, [key]: value })
+    }
     return (
         <div>
             <h1>Přidat subkategorii</h1>
-            
+
         </div>
     )
 }
