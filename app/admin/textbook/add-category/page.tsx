d494a9cb761bca8a5b6ddeee064b587f200a75a0
sanyl <homolkadam@seznam.cz>
2024-04-08 19:55:16 +0200

    Added categories fetch

--- app/admin/textbook/add-category/page.tsx
+++ app/admin/textbook/add-category/page.tsx
@@ -1,18 +1,31 @@
 //@ts-nocheck
 "use client"
 
-import { useState } from "react"
+import { supabase } from "@/api";
+import { useEffect, useState } from "react"
 
 const CategoryAddPage = () => {
-    const [subcategory, setSubcategory] = useState();
+    const [subcategory, setSubcategory] = useState({});
+    const [categories, setCategories] = useState([]);
     const saveSubcategory = (key, value) => {
         setSubcategory({ ...test, [key]: value })
     }
+    const fetchCategories = async () => {
+        const { data, error } = await supabase
+            .from('ucebnice_categories')
+            .select('*')
+        if (error) alert(error.message);
+        setCategories(data);
+    }
+    useEffect(() => {
+        fetchCategories();
+    }, [])
     return (
         <div>
             <h1>Přidat subkategorii</h1>
             ID<input type="text" onChange={e => saveSubcategory("id", e.target.value)} />(zobrazí se jako URL - sanyl.cz/nová-kategorie/)
             Název <input type="text" onChange={e => saveSubcategory("name", e.target.value)} />
+            
         </div>
     )
 }
