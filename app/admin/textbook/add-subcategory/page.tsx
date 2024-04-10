bdf6db5cd5840a203fee271e3191399e945ad774
sanyl <homolkadam@seznam.cz>
2024-04-10 18:25:37 +0200

    Added priorities fetch

--- app/admin/textbook/add-subcategory/page.tsx
+++ app/admin/textbook/add-subcategory/page.tsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from "react"
 const SubcategoryAddPage = () => {
     const [subcategory, setSubcategory] = useState({});
     const [categories, setCategories] = useState([]);
+    const [priorities, setPriorities] = useState([]);
     const router = useRouter();
     const saveSubcategory = (key, value) => {
         setSubcategory({ ...subcategory, [key]: value })
@@ -26,8 +27,16 @@ const SubcategoryAddPage = () => {
         if (error) alert(error.message);
         setCategories(data);
     }
+    const fetchPriorities = async () => {
+        const { data, error } = await supabase
+            .from('ucebnice_subcategories')
+            .select('name, order_priority')
+        if (error) alert(error.message);
+        else setPriorities(data);
+    }
     useEffect(() => {
         fetchCategories();
+        fetchPriorities();
     }, [])
     return (
         <div>
@@ -40,10 +49,15 @@ const SubcategoryAddPage = () => {
                     <option key={category.id} value={category.id}>{category.name}</option>
                 ))}
             </select> <br />
-
+            Priorita <input type="number" onChange={e => saveSubcategory("order_priority", e.target.value)} />
+            <select>
+                {priorities?.map(category => (
+                    <option key={category.name}>{category.name} - {category.order_priority}</option>
+                ))}
+            </select> <br />
             <button onClick={insertSubcategory} className="btn btn-success">Přidat</button>
         </div>
     )
 }
 
-export default CategoryAddPage
+export default SubcategoryAddPage
