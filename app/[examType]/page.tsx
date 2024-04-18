05058f17aa6df9ea37e639d71b8a43dd2b81d95b
sanyl <homolkadam@seznam.cz>
2024-04-18 20:00:26 +0200

    Added article iteration

--- app/[examType]/page.tsx
+++ app/[examType]/page.tsx
@@ -7,15 +7,29 @@ export const getContent = async (examType: string) => {
 }
 
 const ExamPage = async ({ params }: { params: { examType: string } }) => {
+  interface Article {
+    title: string,
+    content: string
+  }
   const page = await getContent(params.examType);
   console.log(page);
   return (
     <div className="">
-      <div className="main-title pb-5 ms-5 me-5 mt-3" style={{ position: "absolute" }}>
+      <div className="main-title pb-5 ms-5 me-5 mt-3">
         <div>
           <h1 className="fw-bold text-blue-2">{page.title}</h1>
           <hr className="text-blue-1 w-auto" />
-          <h2>{page.description}</h2>
+          <h2 className="fs-4">{page.description}</h2>
+        </div>
+
+        <div className="mt-5">
+          {page.articles?.map((article: Article) => (
+            <div key={article.title} className="">
+              <h2 className="fw-bold text-blue-2">{article.title}</h2>
+              <hr className="text-blue-1 w-auto" />
+              <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
+            </div>
+          ))}
         </div>
       </div>
     </div>
