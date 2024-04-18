ed0f3656defaf8196522196d6b058f0335c06909
sanyl <homolkadam@seznam.cz>
2024-04-18 19:08:39 +0200

    Added JSX base

--- app/[examType]/page.tsx
+++ app/[examType]/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import Link from "next/link"
+import Image from "next/image"
 
-const ExamPage = () => {
+export const getContent = async () => {
+
+}
+
+const ExamPage = async () => {
   return (
-    <div>ExamPage</div>
+    <div className="d-flex flex-column main align-items-center">
+
+      <section className="shadow-lg w-100">
+        <div className="d-flex flex-column justify-content-center mobileImageWrapper"
+          style={{ height: "100vh", position: "relative" }}>
+          <div className="main-title" style={{ position: "absolute" }}>
+            <div>
+              <h1 className="fw-bold">Moderní a efektivní příprava</h1>
+              <h2>na přijímací zkoušky a maturitu</h2>
+            </div>
+            <div className="d-flex flex-row justify-content-start">
+              <Link href="/procvicovani" className="btn btn-dark shadow-lg rounded-5 my-3 mx-1 p-2">Začít procvičovat</Link>
+              <a className="btn btn-dark shadow-lg rounded-5 my-3 mx-1 p-2 w-auto px-3" href="#about">fsdfsdf</a>
+            </div>
+          </div>
+          <div style={{ position: "absolute", bottom: "10vh" }}
+            className="container-fluid d-flex align-items-center justify-content-start">
+            <div className="mx-5" v-if="answeredExerciseCount">
+              <span className="fs-1 fw-bold">fsdfsdf</span><span className="fs-6"> vyplněných cvičení</span>
+            </div>
+            <div className="mx-5" v-if="submittedTestCount">
+              <span className="fs-1 fw-bold">fsdfsdf</span><span className="fs-6"> vyplněných testů</span>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div >
   )
 }
 
