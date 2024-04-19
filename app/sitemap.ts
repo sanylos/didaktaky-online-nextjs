5c37934365f4e8f7a10f043990c56bbca3fcbb6d
sanyl <homolkadam@seznam.cz>
2024-04-19 19:43:22 +0200

    Added dropdown links generation

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,11 +1,21 @@
+//@ts-nocheck
 import { MetadataRoute } from 'next'
 import { links } from '@/app/data/links'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    return [
-        ...links.map(link => ({
-            url: process.env.NEXT_PUBLIC_PRODUCTION_URL + '/' + link.href,
-            lastModified: new Date()
-        }))
+    const sitemap: MetadataRoute.Sitemap = [
+        ...links.map(link => (
+            {
+                url: process.env.NEXT_PUBLIC_PRODUCTION_URL + '/' + link.href,
+                lastModified: new Date()
+            }
+        )),
+        ...links.filter(link => link.dropdown).flatMap(link => link.dropdown?.map(dropdownLink => (
+            {
+                url: process.env.NEXT_PUBLIC_PRODUCTION_URL + '/' + link.href + '/' + dropdownLink.href,
+                lastModified: new Date()
+            }
+        ))),
     ]
+    return sitemap
 }
