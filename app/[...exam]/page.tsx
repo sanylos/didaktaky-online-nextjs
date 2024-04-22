df4331950706088c679341fb88eb1187672c3580
sanyl <homolkadam@seznam.cz>
2024-04-22 19:23:16 +0200

    Added page conditional rendering

--- app/[...exam]/page.tsx
+++ app/[...exam]/page.tsx
@@ -58,41 +58,43 @@ const ExamPage = async ({ params }: { params: { exam: string[] } }) => {
   //console.log(page)
   return (
     <div className="">
-      <div className="main-title pb-5 ms-5 me-5 mt-3">
-        <div>
-          <h1 className="fw-bold text-blue-2">{page.content.title}</h1>
-          <hr className="text-blue-1 w-auto" />
-          <h2 className="fs-4">{page.content.description}</h2>
-        </div>
+      {page ?
+        <div className="main-title pb-5 ms-5 me-5 mt-3">
+          <div>
+            <h1 className="fw-bold text-blue-2">{page.content.title}</h1>
+            <hr className="text-blue-1 w-auto" />
+            <h2 className="fs-4">{page.content.description}</h2>
+          </div>
 
-        <div className="mt-5">
-          {page.content.articles?.map((article: Article) => (
-            <div key={article.content} className="">
-              <h2 className="fw-bold text-blue-2">{article.title}</h2>
-              <hr className="text-blue-1 w-auto mt-0" />
-              <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
-            </div>
-          ))}
-        </div>
+          <div className="mt-5">
+            {page.content.articles?.map((article: Article) => (
+              <div key={article.content} className="">
+                <h2 className="fw-bold text-blue-2">{article.title}</h2>
+                <hr className="text-blue-1 w-auto mt-0" />
+                <p dangerouslySetInnerHTML={{ __html: article.content }}></p>
+              </div>
+            ))}
+          </div>
 
 
-        {page.content.faq?.map((question: Question, index: number) => (
-          <div key={index} className="accordion mb-1" id={'accordion' + index}>
-            <div className="accordion-item">
-              <h2 className="accordion-header">
-                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={'#collapse' + index} aria-expanded="true" aria-controls={'collapse' + question.id}>
-                  <span dangerouslySetInnerHTML={{ __html: question.question }}></span>
-                </button>
-              </h2>
-              <div id={'collapse' + index} className="accordion-collapse collapse" data-bs-parent="#accordionQuestions">
-                <div className="accordion-body">
-                  <p dangerouslySetInnerHTML={{ __html: question.answer }}></p>
+          {page.content.faq?.map((question: Question, index: number) => (
+            <div key={index} className="accordion mb-1" id={'accordion' + index}>
+              <div className="accordion-item">
+                <h2 className="accordion-header">
+                  <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target={'#collapse' + index} aria-expanded="true" aria-controls={'collapse' + question.id}>
+                    <span dangerouslySetInnerHTML={{ __html: question.question }}></span>
+                  </button>
+                </h2>
+                <div id={'collapse' + index} className="accordion-collapse collapse" data-bs-parent="#accordionQuestions">
+                  <div className="accordion-body">
+                    <p dangerouslySetInnerHTML={{ __html: question.answer }}></p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div >
+          ))}
+        </div >
+        : "error"}
     </div >
   )
 }
